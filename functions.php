0a3382d08012dfac72f6d0196f85fffc51fe13d4
Patrick Siegfried <siegfried.patrick@gmail.com>
2022-04-25 18:16:12 +0200

    require plugins

--- functions.php
+++ functions.php
@@ -54,7 +54,7 @@ try {
 |
 */
 
-collect(['setup', 'filters'])
+collect(['setup', 'filters', 'lib/require_plugins/require_plugins'])
     ->each(function ($file) {
         if (! locate_template($file = "app/{$file}.php", true, true)) {
             wp_die(
