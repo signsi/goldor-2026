0a6002d38b3dda67435a3d4e9048c4ce3f776e32
Luca Rava <lucarava@mbp-von-luca.home>
2022-04-26 15:02:37 +0200

    Definition Default-Values of block helper components

--- resources/views/blocks/helpers/block-wrapper.blade.php
+++ resources/views/blocks/helpers/block-wrapper.blade.php
@@ -1,15 +1,14 @@
 @php
+    $ignoreAnimation = $ignoreAnimation ?? false;
+    $element_classes = $element_classes ?? false;
 
-$ignoreAnimation = isset($ignoreAnimation) ? $ignoreAnimation : false;
-$element_classes = isset($element_classes) ? $element_classes : '';
+    $block_config = block_config();
+    $div_class = $block_config['name'];
+    $div_class .= App\mapToKeyString(['padding-top', 'padding-right', 'padding-bottom', 'padding-left', 'margin-top', 'margin-right', 'margin-bottom', 'margin-left', 'className']);
+    $div_class .= App\getAnimation($ignoreAnimation);
 
-$block_config = block_config();
-$div_class = $block_config['name'];
-$div_class .= App\mapToKeyString(['padding-top', 'padding-right', 'padding-bottom', 'padding-left', 'margin-top', 'margin-right', 'margin-bottom', 'margin-left', 'className']);
-$div_class .= App\getAnimation($ignoreAnimation);
-
-$div_class .= $element_classes ? ' ' . $element_classes : '';
-$hide = block_value('hide-element');
+    $div_class .= $element_classes ? ' ' . $element_classes : '';
+    $hide = block_value('hide-element');
 @endphp
 
 @if (!$hide)
