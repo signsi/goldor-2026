b0ab9d71e6540eb101966d219c06d89ab7816971
Patrick Siegfried <siegfried.patrick@gmail.com>
2022-06-28 15:02:35 +0200

    zindex

--- resources/scripts/app.js
+++ resources/scripts/app.js
@@ -1,6 +1,5 @@
 import { domReady } from '@roots/sage/client';
-import isVisible from './helpers';
-// import './rocketpager/index.js';
+import { setupMobileNav, setupSubMenus } from './nav';
 
 /**
  * app.main
@@ -15,64 +14,9 @@ const main = async (err) => {
 
 
   // desktop sub menus
-
-  const openSubMenu = (childContainer) => {
-    childContainer.classList.add(...classesShown);
-    childContainer.classList.remove(...classesHidden);
-  }
-
-  const closeSubMenu = (childContainer) => {
-    childContainer.classList.remove(...classesShown);
-    childContainer.classList.add(...classesHidden);
-  }
-
-  const topNav = document.querySelectorAll("#topNav")[0];
-  const submMenuParents = topNav.querySelectorAll("ul>li.menu-item-has-children");
-  const classesShown = ['opacity-1', 'translate-y-0'];
-  const classesHidden = ['opacity-0', 'translate-y-1'];
-
-  submMenuParents.forEach(item => {
-    item.addEventListener('click', (e) => {
-      // wir versichern uns, dass wir direkt auf das Elternelement
-      // und nicht auf Kinderelemente geklickt haben.
-      const isDirectClick = e.target.parentElement.tagName === "DIV"
-      if (isDirectClick) {
-        const childContainer = item.querySelectorAll(":scope > div")[1];
-        const subMenuOpen = isVisible(childContainer);
-
-        if (!subMenuOpen) {
-          closeSubMenu(childContainer);
-        } else {
-          openSubMenu(childContainer);
-        }
-
-        e.preventDefault();
-        return false;
-      }
-    })
-  });
-
-
+  setupSubMenus();
   // mobile toggle
-
-  const mobileNavButton = document.querySelectorAll("#mobileToggle")[0];
-  const mobileNavClose = document.querySelectorAll("#mobileClose")[0];
-  const mobileNav = document.querySelectorAll("#mobileNav")[0];
-  const mobileClassesHidden = ['opacity-0', 'scale-95', '-z-10'];
-  const mobileClassesShown = ['opacity-1', 'scale-100'];
-
-  if (mobileNavButton) {
-    mobileNavButton.addEventListener('click', (e) => {
-      mobileNav.classList.remove(...mobileClassesHidden);
-      mobileNav.classList.add(...mobileClassesShown);
-    })
-  }
-  if (mobileNavClose) {
-    mobileNavClose.addEventListener('click', (e) => {
-      mobileNav.classList.add(...mobileClassesHidden);
-      mobileNav.classList.remove(...mobileClassesShown);
-    })
-  }
+  setupMobileNav();
 
 
 };
