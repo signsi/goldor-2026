de623be110d5f45f1308a53a793d4bda2bda8537
vm-02.dev-rocket.ch <root@dev-rocket.ch>
2022-07-07 17:10:42 +0200

    Definieren Variablen und Abstände

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,40 @@
 module.exports = {
   content: ['./app/**/*.php', './resources/**/*.{php,vue,js}'],
   theme: {
+    screens: {
+      'tablet': '768px',
+      'dekstop': '1280px',
+      'full-hd': '1700px',
+    },
     extend: {
       colors: {
         'primary': '#ff0096',
         'secondary': '#F1D9E7'
       },
       maxWidth: {
-        'content': '90rem',
+        'slimmer': '37.5rem', //600px
+        'slim': '47rem',      //752px
+        'large': '71.25',     //1140px
+        'xlarge': '90rem',    //1440px
+        'content': '90rem',   //1440px
       },
       fontFamily: {
         'serif': ['Bodoni Moda', 'serif'],
         'sans': ['Montserrat', 'Arial', 'sans-serif'],
       },
       spacing: {
-        'luca': '3rem'
+        // Abstände zwischen den verschiedenen Sections
+        'section-mobile': '30px',
+        'section-tablet': '40px',
+        'section-desktop': '60px',
+        'section-full-hd': '90px',
+        // Abstände von Elementen (RocketPager, Bilder Tabellen usw.) innerhalb einer Section
+        'element-mobile': '30px',
+        'element-tablet': '40px',
+        'element-desktop': '60px',
+        // Gutter Abstände z.b. Links und Rechts von Alignwide Blöcken oder Abstand zwischen Kinderelementen
+        'gutter-mobile': '20px',
+        'gutter-display': '30px',
       },
     },
   },
