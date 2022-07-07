c872cc24d94427c8787c2084fd69ebed7d5bed19
vm-02.dev-rocket.ch <root@dev-rocket.ch>
2022-07-07 10:30:10 +0200

    widgets working

--- functions.php
+++ functions.php
@@ -53,8 +53,8 @@ try {
 | is registered alongside Sage.
 |
 */
-// , 'lib/require_plugins/require_plugins'
-collect(['setup', 'filters'])
+
+collect(['setup', 'filters', 'lib/require_plugins/require_plugins'])
     ->each(function ($file) {
         if (! locate_template($file = "app/{$file}.php", true, true)) {
             wp_die(
