c2b533eb6e2cccbf55456911c74c1939a9163c00
vm-02.dev-rocket.ch <root@dev-rocket.ch>
2022-07-07 09:11:01 +0200

    added plugin libs

--- app/lib/require_plugins/require_plugins.php
+++ app/lib/require_plugins/require_plugins.php
@@ -248,28 +248,6 @@ function rocketpacker_register_required_plugins()
 			'force_activation'   => false, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
 			'force_deactivation' => false, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
 		),
-		array(
-			'name'               => 'mmenu', // The plugin name.
-			'slug'               => 'mmenu', // The plugin slug (typically the folder name).
-			'source'             => get_template_directory() . '/lib/mmenu.zip', // The plugin source.
-			'required'           => false, // If false, the plugin is only 'recommended' instead of required.
-			'version'            => '', // E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
-			'force_activation'   => false, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
-			'force_deactivation' => false, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
-			'external_url'       => '', // If set, overrides default API URL and points to an external URL.
-			'is_callable'        => '', // If set, this callable will be be checked for availability to determine if a plugin is active.
-		),
-		array(
-			'name'               => 'WP Bodymovin', // The plugin name.
-			'slug'               => 'wp-bodymovin-master', // The plugin slug (typically the folder name).
-			'source'             => get_template_directory() . '/lib/wp-bodymovin-master.zip', // The plugin source.
-			'required'           => false, // If false, the plugin is only 'recommended' instead of required.
-			'version'            => '', // E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
-			'force_activation'   => false, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
-			'force_deactivation' => false, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
-			'external_url'       => '', // If set, overrides default API URL and points to an external URL.
-			'is_callable'        => '', // If set, this callable will be be checked for availability to determine if a plugin is active.
-		),
 		// This is an example of the use of 'is_callable' functionality. A user could - for instance -
 		// have WPSEO installed *or* WPSEO Premium. The slug would in that last case be different, i.e.
 		// 'wordpress-seo-premium'.
