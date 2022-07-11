913fe64d5b0677fb7d70aae85607d4ce6163c07a
vm-02.dev-rocket.ch <root@dev-rocket.ch>
2022-07-11 10:55:35 +0200

    erste Anpassungen Accordion

--- resources/views/blocks/rocketpager-accordion/block.blade.php
+++ resources/views/blocks/rocketpager-accordion/block.blade.php
@@ -2,13 +2,13 @@
 
 @section('content-section')
     @if(block_rows('element'))
-        <ul class="accordion" data-accordion data-multi-expand="true" data-allow-all-closed="true" data-deep-link="true" data-deep-link-smudge="true" data-deep-link-smudge-delay="600" data-accordion id="deeplinked-accordion-with-smudge">
+        <ul class="accordion bg-transparent mb-0" data-accordion data-multi-expand="true" data-allow-all-closed="true" data-deep-link="true" data-deep-link-smudge="true" data-deep-link-smudge-delay="600" data-accordion id="deeplinked-accordion-with-smudge">
             @while (block_rows('element'))
                 @php block_row('element') @endphp
                 @if(!block_sub_value('hide-element'))
-                    <li class="accordion-item{{ App\getAccordionActive(block_row_index()) }}" data-accordion-item>
-                        <a href="#{{ App\echoDeepLinktitle() }}" class="accordion-title">{{ block_sub_value('title') }}</a>
-                        <div class="accordion-content" data-tab-content id="{{ App\echoDeepLinktitle() }}">
+                    <li class="accordion-item pl-0 block before:hidden{{ App\getAccordionActive(block_row_index()) }}" data-accordion-item>
+                        <a href="#{{ App\echoDeepLinktitle() }}" class="accordion-title color-font border-b border-b-font py-[10px] pr-0 pl-[25px] md:py-[20px]">{{ block_sub_value('title') }}</a>
+                        <div class="accordion-content  bg-transparent border-none p-3 pb-5 pr-0 md:p-5 md:pb-10 md:pr-0" data-tab-content id="{{ App\echoDeepLinktitle() }}">
                             {!! App\sanitize_out(block_sub_value('content'), 'text_area' ) !!}
                         </div>
                     </li>
