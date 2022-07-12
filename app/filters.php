fa45c680ce290f0fdc9b7a29a9260cad44286dc2
Patrick Siegfried <siegfried.patrick@gmail.com>
2022-07-12 08:49:31 +0200

    trying to add group class

--- app/filters.php
+++ app/filters.php
@@ -19,3 +19,19 @@ add_filter('excerpt_more', function () {
 //     // print_r($block['attrs']['className']);
 //     return "<pre>". $block_content . "</pre>";
 // }, 10, 2);
+
+// add_filter('render_block_core/post-template', function ($block_content, $block) {
+//     // print_r($block);
+//     // return '<!--' . $block_content . '-->';
+//     echo '<!-- BBBLOCK' . $block_content . '-->';
+//     return $block_content . ' AAA';
+// }, 10, 2);
+
+
+
+add_filter('render_block_core/query', function ($block_content, $block) {
+    // print_r($block);
+    // return '<!--' . $block_content . '-->';
+    echo '<!-- BBBLOCK' . $block_content . '-->';
+    return $block_content . ' AAA';
+}, 10, 2);
--- resources/scripts/backend/custom-styles.js
+++ resources/scripts/backend/custom-styles.js
@@ -5,6 +5,15 @@ wp.blocks.registerBlockStyle('core/quote', {
     label: 'Fancy Quote',
 });
 
+// https://wordpress.stackexchange.com/questions/308021/how-to-add-a-custom-css-class-to-core-blocks-in-gutenberg-editor
+// not working, non-default block?
+// wp.blocks.registerBlockStyle('core/wp-block-post', {
+//     name: 'fancy-post',
+//     label: 'Fancy Post',
+//     isDefault: true
+// });
+
+
 
 // https://github.com/WordPress/gutenberg/blob/c679f23774a0629a881a04df6102324ef38046f0/packages/block-library/src/query/variations.js
 const QUERY_DEFAULT_ATTRIBUTES = {
@@ -34,9 +43,9 @@ wp.blocks.registerBlockVariation(
         name: 'post-image-grid',
         title: 'Post-Image Grid',
         // icon: '',
-        attributes: { 
+        attributes: {
             ...QUERY_DEFAULT_ATTRIBUTES,
-         },
+        },
         innerBlocks: [
             [
                 'core/post-template',
@@ -59,3 +68,45 @@ wp.blocks.registerBlockStyle('core/query', {
     name: 'post-image-grid',
     label: 'Post-Image Grid',
 });
+
+// const { createHigherOrderComponent } = wp.compose
+
+// const withCustomClassName = createHigherOrderComponent((BlockListBlock) => {
+//     return props => {
+//         return <BlockListBlock {...props} className={'my-custom-class'} />
+//     }
+// }, 'withCustomClassName')
+// wp.hooks.addFilter('editor.BlockListBlock', 'my-plugin/with-custom-class-name', withCustomClassName)
+
+
+// https://github.com/WordPress/gutenberg/blob/3383af2e4a6547987db2186f18d45b34b60e8543/packages/block-library/src/post-template/edit.js
+
+// wp.hooks.addFilter(
+//     'blocks.getBlockDefaultClassName',
+//     'my-plugin/add-block-custom-class-name',
+//     (className) => {
+//         console.log("className", className)
+//         if(className === 'wp-block-post') {
+//             console.log(className);
+//         }
+//         // return 'wppp ' + className;
+//         return className;
+//     }
+// );
+
+
+// https://github.com/WordPress/gutenberg/blob/2caadbfdc578df1c0545fdcf2ee6e528312317a7/packages/block-library/src/post-template/index.php
+
+
+// function addBlockClassName(props, blockType) {
+//     if (blockType.name === 'core/post-template') {
+//         return Object.assign(props, { className: 'wp-bbblock-list' });
+//     }
+//     return props;
+// }
+
+// wp.hooks.addFilter(
+//     'blocks.getSaveContent.extraProps',
+//     'gdt-guten-plugin/add-block-class-name',
+//     addBlockClassName
+// );
