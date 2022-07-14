c933ada10393023ea77a760d74ba582c52db92bf
vm-02.dev-rocket.ch <root@dev-rocket.ch>
2022-07-14 10:22:26 +0200

    JS RocketPager Hero Slider

--- resources/scripts/blocks/rocketpager-hero-slider.js
+++ resources/scripts/blocks/rocketpager-hero-slider.js
@@ -1,7 +1,12 @@
 $('.hero-slider').slick({
-    dots: true,
+    dots: false,
     arrows: false,
     infinite: true,
     slidesToShow: 1,
     slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3500, //Time to show ()
+    fade: true,
+    cssEase: 'ease-in-out',
+    speed: 1000 //Time of Transition
 });
