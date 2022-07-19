a728950e8c6c83ef77f85d38404a57ef767985a2
Patrick Siegfried <siegfried.patrick@gmail.com>
2022-07-19 07:55:04 +0200

    so far, working like a charm

--- shell_scripts/db-sync.sh
+++ shell_scripts/db-sync.sh
@@ -1,1 +1,6 @@
 wp search-replace --allow-root ${LOCAL_URL} ${REMOTE_URL} --export=vm-db-export.sql && mv vm-db-export.sql /var/www/html/export/vm-db-export.sql
+# upload dump
+# import dump
+# rsync data
+wp --allow-root --ssh=devrock@80.74.154.66:2121/httpdocs plugin list 
+# ssh -p $SSH_PORT $SSH_USER@$SSH_HOST "cd $WEB_ROOT && ls"
