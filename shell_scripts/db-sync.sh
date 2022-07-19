99ad8ebe52fa2e3f448b3cf3986d0cf0fd4177d2
Patrick Siegfried <siegfried.patrick@gmail.com>
2022-07-19 08:21:05 +0200

    issues with prefixes

--- shell_scripts/db-sync.sh
+++ shell_scripts/db-sync.sh
@@ -10,5 +10,10 @@ rsync -arvz -e "ssh -p $SSH_PORT" --progress --delete  dump.zip $SSH_USER@$SSH_H
 ssh -p $SSH_PORT $SSH_USER@$SSH_HOST "cd $WEB_ROOT; unzip -o dump.zip; rm dump.zip;"
 # import dump
 # rsync data
-wp --allow-root --ssh=devrock@80.74.154.66:2121/httpdocs plugin list 
+wp --allow-root --ssh=devrock@80.74.154.66:2121/httpdocs plugin list
+echo "dropping db..."
+wp --allow-root --ssh=devrock@80.74.154.66:2121/httpdocs db drop --yes
+echo "dropped db, importing new dump..."
+wp --allow-root --ssh=devrock@80.74.154.66:2121/httpdocs db import vm-db-export.sql
+echo "dump imported."
 # ssh -p $SSH_PORT $SSH_USER@$SSH_HOST "cd $WEB_ROOT && ls"
