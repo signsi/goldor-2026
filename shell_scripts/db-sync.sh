311e3924544751dea4324147af1ec29ae578f8b0
Patrick Siegfried <siegfried.patrick@gmail.com>
2022-07-19 08:09:24 +0200

    work in progress, some issues

--- shell_scripts/db-sync.sh
+++ shell_scripts/db-sync.sh
@@ -1,5 +1,12 @@
+# create db export
 wp search-replace --allow-root ${LOCAL_URL} ${REMOTE_URL} --export=vm-db-export.sql && mv vm-db-export.sql /var/www/html/export/vm-db-export.sql
+# zip dump
+zip dump.zip /var/www/html/export/vm-db-export.sql
 # upload dump
+cat $WEB_ROOT
+rsync -arvz -e "ssh -p $SSH_PORT" --progress --delete  dump.zip $SSH_USER@$SSH_HOST:$WEB_ROOT
+# unzip dump
+ssh -p $SSH_PORT $SSH_USER@$SSH_HOST "cd $WEB_ROOT; unzip dump.zip"
 # import dump
 # rsync data
 wp --allow-root --ssh=devrock@80.74.154.66:2121/httpdocs plugin list 
