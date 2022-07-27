b4da9b077435f4ea652c157ae9a4c3b259dd8c5b
Patrick Siegfried <siegfried.patrick@gmail.com>
2022-07-27 10:22:51 +0200

    cleaning up

--- shell_scripts/db-export.sh
+++
@@ -1,2 +0,0 @@
-cd ./export
-wp db --allow-root export dump.sql
--- shell_scripts/startup.sh
+++
@@ -1,5 +0,0 @@
-docker-compose up -d wordpress db
-echo "Warte auf den Start der Wordpress-Installation."
-sleep 10;
-docker-compose run wpcli wp core install --url=localhost:8007 --title="RocketPager" --admin_name=admin --admin_password=admin --admin_email=psiegfried@rocket.ch --skip-email
-docker-compose run wpcli wp option set blog_public 0
