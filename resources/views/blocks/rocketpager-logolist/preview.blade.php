866d2c1a565d18f0db0e8fce6bdcb115f207058e
vm-02.dev-rocket.ch <root@dev-rocket.ch>
2022-08-03 09:32:28 +0200

    Styling RocketPager Logoliste Preview

--- resources/views/blocks/rocketpager-logolist/preview.blade.php
+++ resources/views/blocks/rocketpager-logolist/preview.blade.php
@@ -1,18 +1,21 @@
-@extends('blocks.helpers.preview-wrapper', ['flex_type' => 'four-columns'])
+@php
+    $flex_type = App\setColumns(true);
+@endphp
 
-<{{ block_field('heading') }}>{{ block_field('title') }}</{{ block_field('heading') }}>
+@extends('blocks.helpers.preview-wrapper', ['flex_type' => $flex_type])
+
+@section('content-section-before-flex')
+    <{{ block_field('heading') }} class="mb-8">{{ block_field('title') }}</{{ block_field('heading') }}>
+@overwrite
+
 @section('flex-item-content')
     @while (block_rows('logo'))
         @php block_row('logo') @endphp
-        <div class="col">
-            <div class="image-wrapper">
-                @include('blocks.helpers.image',
-                [
-                    'name_ImageField' => 'image',
-                    'isRepeaterElement' => true
-                ])
-            </div>
-        </div>
+            @include('blocks.helpers.image',
+            [
+                'name_ImageField' => 'image',
+                'isRepeaterElement' => true
+            ])
     @endwhile
     {{ reset_block_rows('logo') }}
 @overwrite
