32978a958dffdcf8c917ec8a5dd1122807740959
vm-02.dev-rocket.ch <root@dev-rocket.ch>
2022-09-06 13:45:31 +0200

    News und Veranstaltungen

--- resources/views/partials/top/sticky-cta.blade.php
+++ resources/views/partials/top/sticky-cta.blade.php
@@ -11,15 +11,15 @@ $search_active = App\getThemeOption('cta_search');
 
 
 @if ($fixed_cta_active)
-    <div class="sticky-cta show-for-large">
+    <div class="sticky-cta hidden lg:block bottom-auto top-1/2 -translate-y-[50%] w-auto z-20 fixed transition-all">
         <div class="sticky-cta-inner">
             @if ($language_switcher_active)
                 @if (function_exists('pll_the_languages'))
                     @if ($lang_switch_position !== 'hide')
-                        <div class="element-wrapper-lang">
-                            <i class="fal fa-globe"></i>
-                            <div class="content-wrapper">
-                                <ul class="lang-switcher ">
+                        <div class="element-wrapper-lang element-wrapper block mb-2 bg-primary text-white h-[60px] p-0 relative outline-none">
+                            <i class="fal fa-globe bg-primary font-light text-white relative min-w-[60px] h-[60px] font-base transition-colors flex justify-center items-center"></i>
+                            <div class="content-wrapper absolute top-0 transition-all translate-x-0 flex items-center bg-white h-[60px] l-[60px]">
+                                <ul class="lang-switcher m-0 list-none flex">
                                     <?php pll_the_languages([
                                         'show_flags' => 0,
                                         'show_names' => 1,
@@ -33,23 +33,23 @@ $search_active = App\getThemeOption('cta_search');
                 @endif
             @endif
             @if ($search_active)
-                <a class="element-wrapper" data-toggle="modal-search">
-                    <i class="fal fa-search"></i>
+                <a class="element-wrapper block mb-2 bg-primary text-white h-[60px] p-0 relative outline-none" data-toggle="modal-search">
+                    <i class="fal fa-search bg-primary font-light text-white relative min-w-[60px] h-[60px] font-base transition-colors flex justify-center items-center"></i>
                 </a>
             @endif
 
             @if ($link_active)
-                <a class="element-wrapper" href="{!! $link_url !!}" target="_blank">
-                    <i class="fal fa-envelope"></i>
-                    <div class="content-wrapper">
+                <a class="element-wrapper block mb-2 bg-primary text-white h-[60px] p-0 relative outline-none" href="{!! $link_url !!}" target="_blank">
+                    <i class="fal fa-envelope bg-primary font-light text-white relative min-w-[60px] h-[60px] font-base transition-colors flex justify-center items-center"></i>
+                    <div class="content-wrapper absolute top-0 transition-all translate-x-0 flex items-center bg-white h-[60px] l-[60px]">
                         <p>{!! $link_text !!}</p>
                     </div>
                 </a>
             @endif
             @if ($social_share_active)
-                <div class="element-wrapper">
-                    <i class="fal fa-share-alt"></i>
-                    <div class="content-wrapper social-share">
+                <div class="element-wrapper block mb-2 bg-primary text-white h-[60px] p-0 relative outline-none">
+                    <i class="fal fa-share-alt bg-primary font-light text-white relative min-w-[60px] h-[60px] font-base transition-colors flex justify-center items-center"></i>
+                    <div class="content-wrapper absolute top-0 transition-all translate-x-0 flex items-center bg-white h-[60px] l-[60px] social-share">
                         @include('partials.social-share')
                     </div>
                 </div>
