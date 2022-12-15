f044a309744098a8cadf3cc8c0718daab29cbb07
vm-02.dev-rocket.ch <root@dev-rocket.ch>
2022-12-15 10:10:23 +0100

    Bug-Fix Page-Parital

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
--    @if (! have_posts())
--        @include('partials.content.content-none')
--    @else
--        @while (have_posts())
--            @php(the_post())
--            @includeFirst(['partials.content.content-page', 'partials.content.content'])
--        @endwhile
--    @endif
+    @if (! have_posts())
+        @include('partials.content.content-none')
+    @else
+        @while (have_posts())
+            @php(the_post())
+            @includeFirst(['partials.content.content-page', 'partials.content.content'])
+        @endwhile
+    @endif
 @endsection
