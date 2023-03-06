a4573cd7257f4ce6108b143ce2d49a58a1c13870
Rocker GmbH <online@rocket.ch>
2023-03-06 18:22:14 +0100

    Bug-Fix volle Hoehe bei mobilen Geraeten mit URL & AppBar

--- resources/scripts/app.js
+++ resources/scripts/app.js
@@ -15,6 +15,9 @@ domReady(async (err) => {
     console.error(err);
   }
 
+  let barHeight = Math.max(window.screen.availHeight - window.innerHeight - $('#siteHeader').height() , 0);
+  document.documentElement.style.setProperty('--bar-height', `${barHeight}px`);
+
   handleAnchorJump();
 
   setupNavigation({
