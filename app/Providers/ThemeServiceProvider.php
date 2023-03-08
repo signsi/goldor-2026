095903a1de320c424e4bdb619ddc6b074ce0628c
Rocker GmbH <online@rocket.ch>
2023-03-08 09:17:22 +0100

    aufräumen

--- app/Providers/ThemeServiceProvider.php
+++ app/Providers/ThemeServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Blade;
 use Roots\Acorn\Sage\SageServiceProvider;
 
 class ThemeServiceProvider extends SageServiceProvider
@@ -24,5 +25,24 @@ class ThemeServiceProvider extends SageServiceProvider
     public function boot()
     {
         parent::boot();
+        
+        // Ermöglicht relative Import von Blade-Files
+        // Quelle: https://stackoverflow.com/questions/49894221/laravel-blade-include-files-with-relative-path
+        \Blade::directive('relInclude', function ($args) {
+            $args = Blade::stripParentheses($args);
+
+            $viewBasePath = Blade::getPath();
+            foreach ($this->app['config']['view.paths'] as $path) {
+                if (substr($viewBasePath,0,strlen($path)) === $path) {
+                    $viewBasePath = substr($viewBasePath,strlen($path));
+                    break;
+                }
+            }
+
+            $viewBasePath = dirname(trim($viewBasePath,'\/'));
+            $args = substr_replace($args, $viewBasePath.'.', 1, 0);
+            return "<?php echo \$__env->make({$args}, \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>";
+        });
+
     }
 }
