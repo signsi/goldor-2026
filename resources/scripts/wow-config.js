34e35c8c4f4844f3822c20b8487983f77bc6c5f4
Rocker GmbH <online@rocket.ch>
2023-03-08 11:08:52 +0100

    wow und animate klappen

--- resources/scripts/wow-config.js
+++ resources/scripts/wow-config.js
@@ -4,7 +4,6 @@ export function setupWowAnimation() {
     
     const wow = new WOW();
     wow.init();
-    console.log("wow init", wow);
     $(document).on('DOMNodeInserted','.wow', function() {
         wow.sync();
     });
