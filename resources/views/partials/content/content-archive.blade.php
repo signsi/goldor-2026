19ae884ebef47fa073dc710c02c7a4323f15bc77
Rocker GmbH <online@rocket.ch>
2023-03-08 08:33:46 +0100

    probleme mit custom blade directive

---
+++ resources/views/partials/content/content-archive.blade.php
@@ -0,0 +1,13 @@
+<div class="wp-block-group is-style-layout-full">
+    <div class="wp-block-group">
+    <h1>{{ App\pl__('Archiv - Titel') }}</h1>
+        <div class="grid grid-col-1 md:grid-cols-2 lg:grid-cols-3 gap-gutter">
+            @while(have_posts()) @php(the_post())
+                @includeFirst(['partials.content.content-' . get_post_type(), 'partials.content.content'])
+            @endwhile
+        </div>
+    </div>
+    <div class="wp-block-group mt-element">
+        @include('partials.components.postnavigation')
+    </div>
+</div>
