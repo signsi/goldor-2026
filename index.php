4cc6d4252779cb8ff5317510ecd96a9b9ee245ce
Rocker GmbH <online@rocket.ch>
2023-03-08 11:39:28 +0100

    index cleanup

--- index.php
+++ index.php
@@ -1,20 +1,27 @@
 <!doctype html>
 <html <?php language_attributes(); ?>>
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <?php wp_head(); ?>
-  </head>
 
-  <body <?php body_class(); ?>>
-    <?php wp_body_open(); ?>
-    <?php do_action('get_header'); ?>
+<head>
+  <meta charset="utf-8">
+  <?php wp_head(); ?>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+  <meta name="theme-color" content="#7BA048" />
+  <script src="https://kit.fontawesome.com/9b15eeda8b.js" crossorigin="anonymous"></script>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;500;600;700&display=swap" rel="stylesheet">
+</head>
 
-    <div id="app">
-      <?php echo view(app('sage.view'), app('sage.data'))->render(); ?>
-    </div>
+<body <?php body_class(); ?>>
+  <?php wp_body_open(); ?>
+  <?php do_action('get_header'); ?>
 
-    <?php do_action('get_footer'); ?>
-    <?php wp_footer(); ?>
-  </body>
+  <div id="app">
+    <?php echo view(app('sage.view'), app('sage.data'))->render(); ?>
+  </div>
+
+  <?php do_action('get_footer'); ?>
+  <?php wp_footer(); ?>
+</body>
+
 </html>
