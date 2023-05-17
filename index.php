c4e97ef8cead2da05942dab20509d787e13ede37
Patrick Siegfried <siegfried.patrick@gmail.com>
2023-05-17 10:01:08 +0000

    Update index.php

--- index.php
+++ index.php
@@ -6,10 +6,6 @@
   <?php wp_head(); ?>
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
   <meta name="theme-color" content="#7BA048" />
-  <script src="https://kit.fontawesome.com/9b15eeda8b.js" crossorigin="anonymous"></script>
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@400;700&display=swap" rel="stylesheet">
 </head>
 
 <body <?php body_class(); ?>>
