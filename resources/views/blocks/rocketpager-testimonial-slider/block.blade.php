1fd655d24bb3c66315f87932e3b0f95a29ebd482
Rocket GmbH <online@rocket.ch>
2023-09-26 14:44:58 +0000

    Testimonial Slider

--- resources/views/blocks/rocketpager-testimonial-slider/block.blade.php
+++ resources/views/blocks/rocketpager-testimonial-slider/block.blade.php
@@ -4,7 +4,7 @@
     <div class="testimonial my-element-tablet md:my-element-desktop">
         @while (block_rows('testimonial'))
             @php block_row('testimonial') @endphp
-            <div class="testimonial-itemwrapper !block relative w-full md:w-[80%] max-w-slimmer mx-auto mb-0 text-center">
+            <div class="testimonial-itemwrapper !block relative w-full md:w-[80%] max-w-slim mx-auto text-center mb-element">
                 @if ( block_sub_value('testimonial-link') )
                     <a href="{{ block_sub_value('testimonial-link') }}" target="_blank">
                 @endif
@@ -13,7 +13,7 @@
                     [
                         'name_ImageField' => 'testimonial-logo',
                         'class_object_fill_breakpoint' => 'bg-object-wrapper img-wrapper max-w-[170px] h-[170px] mx-auto mb-5 overflow-hidden rounded-full p-4 bg-white',
-                        'class_object_fit' => array('class' => 'object-contain p-6'),
+                        'class_object_fit' => array('class' => 'object-cover'),
                         'thumbnail' => 'small-width',
                         'isRepeaterElement' => true
                     ])
@@ -26,7 +26,7 @@
                     <p class="testimonial-title mb-3 mx-auto"><strong>{{ block_sub_value('testimonial-companyname') }}</strong></p>
                 @endif
                 @if ( block_sub_value('testimonial-content') )
-                    <p class="mb-0 mx-auto">«{{ block_sub_value('testimonial-content') }}»
+                    <p class="mt-0 mx-auto max-w-prose">«{{ block_sub_value('testimonial-content') }}»
                         @if ( block_sub_value( 'testimonial-name') )
                             - <i>{{ block_sub_value('testimonial-name') }}</i>
                         @endif
