a1c30cdbac8dfbebe0cf74602f19f9adcbfc158b
Patrick Siegfried <psiegfried@rocket.ch>
2023-11-06 16:16:02 +0000

    preview

--- resources/scripts/blocks/rocketpager-bilderwand.js
+++ resources/scripts/blocks/rocketpager-bilderwand.js
@@ -17,13 +17,14 @@ bilderwandItems.forEach(item => {
             // start: "top bottom",
             start: "20% bottom",
             // markers: true,
-            end: "bottom top",
+            end: "60% top",
+            toggleActions: "play none none reverse",
             onEnter: () => {
                 gsap.to(img, { opacity: 1, duration: 0.5 });
             },
-            onLeave: () => {
-                gsap.to(img, { opacity: 0, duration: 0.5 });
-            }
+            // onLeave: () => {
+            //     gsap.to(img, { opacity: 0, duration: 0.5 });
+            // }
         }
     });
 });
--- resources/views/blocks/rocketpager-bilderwand/preview.blade.php
+++ resources/views/blocks/rocketpager-bilderwand/preview.blade.php
@@ -1,1 +1,7 @@
-bilderwand preview
+@extends('blocks.helpers.preview-wrapper', ['flex_type' => 'grid-cols-2'])
+
+@section('flex-item-content')
+    <div class="">
+        {!! App\sanitize_out(block_value('element'), 'allow_iframe') !!}
+    </div>
+@overwrite
