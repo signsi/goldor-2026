d46fa863f4ad86f07bf72d9353165de97f60b70f
Patrick Siegfried <siegfried.patrick@gmail.com>
2023-11-13 07:23:04 +0000

    version

--- app/lib/require_plugins/require_plugins.php
+++ app/lib/require_plugins/require_plugins.php
@@ -63,81 +63,82 @@ function rocketpacker_register_required_plugins()
 
 		// This is an example of how to include a plugin bundled with a theme.
 		/*
-        array(
-            'name'               => 'Rocket Gutenberg Extension',
-            'slug'               => 'rocket-gutenberg-extension', 
-            'source'             => get_template_directory() . '/lib/gutenberg-extension.zip', 
-            'required'           => false, 
-            'version'            => '', 
-            'force_activation'   => false, 
-            'force_deactivation' => true, 
-            'external_url'       => '', // If set, overrides default API URL and points to an external URL.
-            'is_callable'        => '', // If set, this callable will be be checked for availability to determine if a plugin is active.
-		),
-		*/
+			  array(
+				  'name'               => 'Rocket Gutenberg Extension',
+				  'slug'               => 'rocket-gutenberg-extension', 
+				  'source'             => get_template_directory() . '/lib/gutenberg-extension.zip', 
+				  'required'           => false, 
+				  'version'            => '', 
+				  'force_activation'   => false, 
+				  'force_deactivation' => true, 
+				  'external_url'       => '', // If set, overrides default API URL and points to an external URL.
+				  'is_callable'        => '', // If set, this callable will be be checked for availability to determine if a plugin is active.
+			  ),
+			  */
 
 		/*
 
-        // This is an example of how to include a plugin from an arbitrary external source in your theme.
-        array(
-            'name'         => 'TGM New Media Plugin',
-            'slug'         => 'tgm-new-media-plugin', 
-            'source'       => 'https://s3.amazonaws.com/tgm/tgm-new-media-plugin.zip', 
-            'required'     => true, 
-            'external_url' => 'https://github.com/thomasgriffin/New-Media-Image-Uploader', // If set, overrides default API URL and points to an external URL.
-        ),
+			  // This is an example of how to include a plugin from an arbitrary external source in your theme.
+			  array(
+				  'name'         => 'TGM New Media Plugin',
+				  'slug'         => 'tgm-new-media-plugin', 
+				  'source'       => 'https://s3.amazonaws.com/tgm/tgm-new-media-plugin.zip', 
+				  'required'     => true, 
+				  'external_url' => 'https://github.com/thomasgriffin/New-Media-Image-Uploader', // If set, overrides default API URL and points to an external URL.
+			  ),
 
-        // This is an example of how to include a plugin from a GitHub repository in your theme.
-        // This presumes that the plugin code is based in the root of the GitHub repository
-        // and not in a subdirectory ('/src') of the repository.
-        array(
-            'name'      => 'Adminbar Link Comments to Pending',
-            'slug'      => 'adminbar-link-comments-to-pending',
-            'source'    => 'https://github.com/jrfnl/WP-adminbar-comments-to-pending/archive/master.zip',
-        ),
+			  // This is an example of how to include a plugin from a GitHub repository in your theme.
+			  // This presumes that the plugin code is based in the root of the GitHub repository
+			  // and not in a subdirectory ('/src') of the repository.
+			  array(
+				  'name'      => 'Adminbar Link Comments to Pending',
+				  'slug'      => 'adminbar-link-comments-to-pending',
+				  'source'    => 'https://github.com/jrfnl/WP-adminbar-comments-to-pending/archive/master.zip',
+			  ),
 
-        */
+			  */
 		// 8a6ba8ae-acac-42a0-b046-b238b095f88e
 		array(
-			'name'               => 'Genesis Custom Blocks Pro',
-			'slug'               => 'genesis-custom-blocks-pro', 
-			'source'             => get_template_directory() . '/lib/genesis-custom-blocks-pro-1.6.0.zip', 
-			'required'           => false, 
-			'force_activation'   => false, 
-			'force_deactivation' => true, 
+			'name' => 'Genesis Custom Blocks Pro',
+			'version' => '1.6.0',
+			'slug' => 'genesis-custom-blocks-pro',
+			'source' => get_template_directory() . '/lib/genesis-custom-blocks-pro-1.6.0.zip',
+			'required' => false,
+			'force_activation' => false,
+			'force_deactivation' => true,
 		),
 		array(
-			'name'      => 'WPMU DEV Dashboard',
-			'slug'      => 'wpmudev-updates',
-			'source'             => get_template_directory() . '/lib/944810_wpmu-dev-dashboard-4.11.8.zip', 
-			'external_url'		 => 'https://wpmudev.com/project/wpmu-dev-dashboard/',
-			'required'           => false, 
-			'force_activation'   => false, 
-			'force_deactivation' => false, 
+			'name' => 'WPMU DEV Dashboard',
+			'slug' => 'wpmudev-updates',
+			'source' => get_template_directory() . '/lib/944810_wpmu-dev-dashboard-4.11.8.zip',
+			'external_url' => 'https://wpmudev.com/project/wpmu-dev-dashboard/',
+			'required' => false,
+			'force_activation' => false,
+			'force_deactivation' => false,
 		),
 		array(
-			'name'      => 'WP Rocket',
-			'slug'      => 'wp-rocket',
-			'source'             => 'https://wp-rocket.me/download/267124/1e268e64/',
-			'external_url'		 => 'https://wp-rocket.me',
-			'required'           => false, 
-			'force_activation'   => false, 
-			'force_deactivation' => false, 
+			'name' => 'WP Rocket',
+			'slug' => 'wp-rocket',
+			'source' => 'https://wp-rocket.me/download/267124/1e268e64/',
+			'external_url' => 'https://wp-rocket.me',
+			'required' => false,
+			'force_activation' => false,
+			'force_deactivation' => false,
 
 		),
 		array(
-			'name'      => 'Enable Media Replace',
-			'slug'      => 'enable-media-replace',
-			'required'  => false,
-			'force_activation'   => false, 
-			'force_deactivation' => false, 
+			'name' => 'Enable Media Replace',
+			'slug' => 'enable-media-replace',
+			'required' => false,
+			'force_activation' => false,
+			'force_deactivation' => false,
 		),
 		array(
-			'name'      => 'Password Protected',
-			'slug'      => 'password-protected',
-			'required'  => false,
-			'force_activation'   => false, 
-			'force_deactivation' => false, 
+			'name' => 'Password Protected',
+			'slug' => 'password-protected',
+			'required' => false,
+			'force_activation' => false,
+			'force_deactivation' => false,
 		),
 		// array(
 		// 	'name'      => 'Polylang',
@@ -147,32 +148,32 @@ function rocketpacker_register_required_plugins()
 		// 	'force_deactivation' => false, 
 		// ),
 		array(
-			'name'      => 'Yoast Duplicate Post',
-			'slug'      => 'duplicate-post',
-			'required'  => false,
-			'force_activation'   => false, 
-			'force_deactivation' => false, 
+			'name' => 'Yoast Duplicate Post',
+			'slug' => 'duplicate-post',
+			'required' => false,
+			'force_activation' => false,
+			'force_deactivation' => false,
 		),
 		array(
-			'name'      => 'Regenerate Thumbnails',
-			'slug'      => 'regenerate-thumbnails',
-			'required'  => false,
-			'force_activation'   => false, 
-			'force_deactivation' => false, 
+			'name' => 'Regenerate Thumbnails',
+			'slug' => 'regenerate-thumbnails',
+			'required' => false,
+			'force_activation' => false,
+			'force_deactivation' => false,
 		),
 		array(
-			'name'      => 'WebP Converter for Media',
-			'slug'      => 'webp-converter-for-media',
-			'required'  => false,
-			'force_activation'   => false, 
-			'force_deactivation' => false, 
+			'name' => 'WebP Converter for Media',
+			'slug' => 'webp-converter-for-media',
+			'required' => false,
+			'force_activation' => false,
+			'force_deactivation' => false,
 		),
 		array(
-			'name'      => 'SVG Support',
-			'slug'      => 'svg-support',
-			'required'  => false,
-			'force_activation'   => false, 
-			'force_deactivation' => false, 
+			'name' => 'SVG Support',
+			'slug' => 'svg-support',
+			'required' => false,
+			'force_activation' => false,
+			'force_deactivation' => false,
 		),
 		// This is an example of the use of 'is_callable' functionality. A user could - for instance -
 		// have WPSEO installed *or* WPSEO Premium. The slug would in that last case be different, i.e.
@@ -192,93 +193,103 @@ function rocketpacker_register_required_plugins()
 	 * Only uncomment the strings in the config array if you want to customize the strings.
 	 */
 	$config = array(
-		'id'           => 'rocketpager',                 // Unique ID for hashing notices for multiple instances of TGMPA.
-		'default_path' => '',                      // Default absolute path to bundled plugins.
-		'menu'         => 'tgmpa-install-plugins', // Menu slug.
-		'parent_slug'  => 'themes.php',            // Parent menu slug.
-		'capability'   => 'edit_theme_options',    // Capability needed to view plugin install page, should be a capability associated with the parent menu used.
-		'has_notices'  => true,                    // Show admin notices or not.
-		'dismissable'  => true,                    // If false, a user cannot dismiss the nag message.
-		'dismiss_msg'  => '',                      // If 'dismissable' is false, this message will be output at top of nag.
-		'is_automatic' => false,                   // Automatically activate plugins after installation or not.
-		'message'      => '',                      // Message to output right before the plugins table.
+		'id' => 'rocketpager',
+		// Unique ID for hashing notices for multiple instances of TGMPA.
+		'default_path' => '',
+		// Default absolute path to bundled plugins.
+		'menu' => 'tgmpa-install-plugins',
+		// Menu slug.
+		'parent_slug' => 'themes.php',
+		// Parent menu slug.
+		'capability' => 'edit_theme_options',
+		// Capability needed to view plugin install page, should be a capability associated with the parent menu used.
+		'has_notices' => true,
+		// Show admin notices or not.
+		'dismissable' => true,
+		// If false, a user cannot dismiss the nag message.
+		'dismiss_msg' => '',
+		// If 'dismissable' is false, this message will be output at top of nag.
+		'is_automatic' => false,
+		// Automatically activate plugins after installation or not.
+		'message' => '',
+		// Message to output right before the plugins table.
 
 		/*
-		'strings'      => array(
-			'page_title'                      => __( 'Install Required Plugins', 'rocketpager' ),
-			'menu_title'                      => __( 'Install Plugins', 'rocketpager' ),
-			/* translators: %s: plugin name. * /
-			'installing'                      => __( 'Installing Plugin: %s', 'rocketpager' ),
-			/* translators: %s: plugin name. * /
-			'updating'                        => __( 'Updating Plugin: %s', 'rocketpager' ),
-			'oops'                            => __( 'Something went wrong with the plugin API.', 'rocketpager' ),
-			'notice_can_install_required'     => _n_noop(
-				/* translators: 1: plugin name(s). * /
-				'This theme requires the following plugin: %1$s.',
-				'This theme requires the following plugins: %1$s.',
-				'rocketpager'
-			),
-			'notice_can_install_recommended'  => _n_noop(
-				/* translators: 1: plugin name(s). * /
-				'This theme recommends the following plugin: %1$s.',
-				'This theme recommends the following plugins: %1$s.',
-				'rocketpager'
-			),
-			'notice_ask_to_update'            => _n_noop(
-				/* translators: 1: plugin name(s). * /
-				'The following plugin needs to be updated to its latest version to ensure maximum compatibility with this theme: %1$s.',
-				'The following plugins need to be updated to their latest version to ensure maximum compatibility with this theme: %1$s.',
-				'rocketpager'
-			),
-			'notice_ask_to_update_maybe'      => _n_noop(
-				/* translators: 1: plugin name(s). * /
-				'There is an update available for: %1$s.',
-				'There are updates available for the following plugins: %1$s.',
-				'rocketpager'
-			),
-			'notice_can_activate_required'    => _n_noop(
-				/* translators: 1: plugin name(s). * /
-				'The following required plugin is currently inactive: %1$s.',
-				'The following required plugins are currently inactive: %1$s.',
-				'rocketpager'
-			),
-			'notice_can_activate_recommended' => _n_noop(
-				/* translators: 1: plugin name(s). * /
-				'The following recommended plugin is currently inactive: %1$s.',
-				'The following recommended plugins are currently inactive: %1$s.',
-				'rocketpager'
-			),
-			'install_link'                    => _n_noop(
-				'Begin installing plugin',
-				'Begin installing plugins',
-				'rocketpager'
-			),
-			'update_link' 					  => _n_noop(
-				'Begin updating plugin',
-				'Begin updating plugins',
-				'rocketpager'
-			),
-			'activate_link'                   => _n_noop(
-				'Begin activating plugin',
-				'Begin activating plugins',
-				'rocketpager'
-			),
-			'return'                          => __( 'Return to Required Plugins Installer', 'rocketpager' ),
-			'plugin_activated'                => __( 'Plugin activated successfully.', 'rocketpager' ),
-			'activated_successfully'          => __( 'The following plugin was activated successfully:', 'rocketpager' ),
-			/* translators: 1: plugin name. * /
-			'plugin_already_active'           => __( 'No action taken. Plugin %1$s was already active.', 'rocketpager' ),
-			/* translators: 1: plugin name. * /
-			'plugin_needs_higher_version'     => __( 'Plugin not activated. A higher version of %s is needed for this theme. Please update the plugin.', 'rocketpager' ),
-			/* translators: 1: dashboard link. * /
-			'complete'                        => __( 'All plugins installed and activated successfully. %1$s', 'rocketpager' ),
-			'dismiss'                         => __( 'Dismiss this notice', 'rocketpager' ),
-			'notice_cannot_install_activate'  => __( 'There are one or more required or recommended plugins to install, update or activate.', 'rocketpager' ),
-			'contact_admin'                   => __( 'Please contact the administrator of this site for help.', 'rocketpager' ),
+			  'strings'      => array(
+				  'page_title'                      => __( 'Install Required Plugins', 'rocketpager' ),
+				  'menu_title'                      => __( 'Install Plugins', 'rocketpager' ),
+				  /* translators: %s: plugin name. * /
+				  'installing'                      => __( 'Installing Plugin: %s', 'rocketpager' ),
+				  /* translators: %s: plugin name. * /
+				  'updating'                        => __( 'Updating Plugin: %s', 'rocketpager' ),
+				  'oops'                            => __( 'Something went wrong with the plugin API.', 'rocketpager' ),
+				  'notice_can_install_required'     => _n_noop(
+					  /* translators: 1: plugin name(s). * /
+					  'This theme requires the following plugin: %1$s.',
+					  'This theme requires the following plugins: %1$s.',
+					  'rocketpager'
+				  ),
+				  'notice_can_install_recommended'  => _n_noop(
+					  /* translators: 1: plugin name(s). * /
+					  'This theme recommends the following plugin: %1$s.',
+					  'This theme recommends the following plugins: %1$s.',
+					  'rocketpager'
+				  ),
+				  'notice_ask_to_update'            => _n_noop(
+					  /* translators: 1: plugin name(s). * /
+					  'The following plugin needs to be updated to its latest version to ensure maximum compatibility with this theme: %1$s.',
+					  'The following plugins need to be updated to their latest version to ensure maximum compatibility with this theme: %1$s.',
+					  'rocketpager'
+				  ),
+				  'notice_ask_to_update_maybe'      => _n_noop(
+					  /* translators: 1: plugin name(s). * /
+					  'There is an update available for: %1$s.',
+					  'There are updates available for the following plugins: %1$s.',
+					  'rocketpager'
+				  ),
+				  'notice_can_activate_required'    => _n_noop(
+					  /* translators: 1: plugin name(s). * /
+					  'The following required plugin is currently inactive: %1$s.',
+					  'The following required plugins are currently inactive: %1$s.',
+					  'rocketpager'
+				  ),
+				  'notice_can_activate_recommended' => _n_noop(
+					  /* translators: 1: plugin name(s). * /
+					  'The following recommended plugin is currently inactive: %1$s.',
+					  'The following recommended plugins are currently inactive: %1$s.',
+					  'rocketpager'
+				  ),
+				  'install_link'                    => _n_noop(
+					  'Begin installing plugin',
+					  'Begin installing plugins',
+					  'rocketpager'
+				  ),
+				  'update_link' 					  => _n_noop(
+					  'Begin updating plugin',
+					  'Begin updating plugins',
+					  'rocketpager'
+				  ),
+				  'activate_link'                   => _n_noop(
+					  'Begin activating plugin',
+					  'Begin activating plugins',
+					  'rocketpager'
+				  ),
+				  'return'                          => __( 'Return to Required Plugins Installer', 'rocketpager' ),
+				  'plugin_activated'                => __( 'Plugin activated successfully.', 'rocketpager' ),
+				  'activated_successfully'          => __( 'The following plugin was activated successfully:', 'rocketpager' ),
+				  /* translators: 1: plugin name. * /
+				  'plugin_already_active'           => __( 'No action taken. Plugin %1$s was already active.', 'rocketpager' ),
+				  /* translators: 1: plugin name. * /
+				  'plugin_needs_higher_version'     => __( 'Plugin not activated. A higher version of %s is needed for this theme. Please update the plugin.', 'rocketpager' ),
+				  /* translators: 1: dashboard link. * /
+				  'complete'                        => __( 'All plugins installed and activated successfully. %1$s', 'rocketpager' ),
+				  'dismiss'                         => __( 'Dismiss this notice', 'rocketpager' ),
+				  'notice_cannot_install_activate'  => __( 'There are one or more required or recommended plugins to install, update or activate.', 'rocketpager' ),
+				  'contact_admin'                   => __( 'Please contact the administrator of this site for help.', 'rocketpager' ),
 
-			'nag_type'                        => '', // Determines admin notice type - can only be one of the typical WP notice classes, such as 'updated', 'update-nag', 'notice-warning', 'notice-info' or 'error'. Some of which may not work as expected in older WP versions.
-		),
-		*/
+				  'nag_type'                        => '', // Determines admin notice type - can only be one of the typical WP notice classes, such as 'updated', 'update-nag', 'notice-warning', 'notice-info' or 'error'. Some of which may not work as expected in older WP versions.
+			  ),
+			  */
 	);
 
 	tgmpa($plugins, $config);
