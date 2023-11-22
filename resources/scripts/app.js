a3c9dc75f0cd85366c28be5dec032b1ff100d19d
Patrick Siegfried <siegfried.patrick@gmail.com>
2023-11-22 08:42:58 +0000

    0.5s

--- resources/scripts/app.js
+++ resources/scripts/app.js
@@ -21,7 +21,7 @@ gsap.registerPlugin(
 );
 
 ScrollSmoother.create({
-  smooth: 1, // how long (in seconds) it takes to "catch up" to the native scroll position
+  smooth: 0.5, // how long (in seconds) it takes to "catch up" to the native scroll position
   effects: true, // looks for data-speed and data-lag attributes on elements
   // smoothTouch: 0.1, // much shorter smoothing time on touch devices (default is NO smoothing on touch devices)
 });
