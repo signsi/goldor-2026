2fb2a9d3bc5fe7f7218faee79d4d8798c61ed5ed
Tim Kägi <tkaegi@rocket.ch>
2023-11-22 15:49:21 +0000

    Spacing Bud

--- bud.config.js
+++ bud.config.js
@@ -89,7 +89,6 @@ export default async (app) => {
             },
             spacing: {
                 blockGap: true,
-                padding: true,
                 margin: true,
                 spacingSizes: [
                     {
