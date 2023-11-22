ec17b0111ee55052c8cfd837bddd40533bfa5ba6
Tim Kägi <tkaegi@rocket.ch>
2023-11-22 15:49:21 +0000

    Spacing Bud

--- bud.config.js
+++ bud.config.js
@@ -85,7 +85,6 @@ export default async (app) => {
             },
             spacing: {
                 blockGap: true,
-                padding: true,
                 margin: true,
                 spacingSizes: [
                     {
--- resources/views/blocks/rocketpager-bilderwand/preview.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('blocks.helpers.preview-wrapper', ['flex_type' => 'grid-cols-2'])
-
-@section('flex-item-content')
-    <div class="">
-        {!! App\sanitize_out(block_value('element'), 'allow_iframe') !!}
-    </div>
-@overwrite
