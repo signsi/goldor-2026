e549ac4b2764467ac4aec9cda6b81fd3119a4a30
Tim Kaegi <tkaegi@rocket.ch>
2023-11-28 15:11:55 +0000

    content-block vorhanden

---
+++ resources/views/components/content-block/mr-spacing-1.blade.php
@@ -0,0 +1,3 @@
+<div class="mr-element-mobile md:mr-element-tablet lg:mr-element-desktop">
+    {{ $slot }}
+</div>
---
+++ resources/views/components/content-block/p-spacing-1.blade.php
@@ -0,0 +1,3 @@
+<div class="p-element-mobile md:p-element-tablet lg:p-element-desktop">
+    {{ $slot }}
+</div>
