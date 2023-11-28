48f68bcdb1f61fdf13dee483734b2e03454d6584
Tim Kaegi <tkaegi@rocket.ch>
2023-11-28 15:13:25 +0000

    Spacings

--- resources/views/components/content-block/ml-spacing-1.blade.php
+++
@@ -1,3 +0,0 @@
-<div class="ml-element-mobile md:ml-element-tablet lg:ml-element-desktop">
-    {{ $slot }}
-</div>
--- resources/views/components/content-block/my-spacing-1.blade.php
+++
@@ -1,3 +0,0 @@
-<div class="my-element-mobile md:my-element-tablet lg:my-element-desktop">
-    {{ $slot }}
-</div>
