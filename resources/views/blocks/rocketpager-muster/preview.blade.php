b10d3de97ff062c60ec592f14c27dcc57b88f1ce
Tim Kaegi <tkaegi@rocket.ch>
2023-11-29 15:50:33 +0000

    groesseres Update, Code-Cleaning

--- resources/views/blocks/rocketpager-muster/preview.blade.php
+++ resources/views/blocks/rocketpager-muster/preview.blade.php
@@ -1,10 +1,7 @@
 {{--
-    Falls es kein Repeater-Feld oder Query-Loop gibt, kann die HTML-Strukur innerhalb von "@section('content-section-before-flex')" und "@endsection" definiert werden.
-    Bei Repeater oder Query-Loop kann das wiederholende Element innerhalb von "@section('flex-item-content')" und "@endsection" definiert werden.
-    Falls vor oder nach den wiederholenden Elementen etwas dargestellt werden soll, kann dies über "@section('content-section-before-flex')" oder "@section('content-section-after-flex')" erreicht werden.
-    Mit dem zusätzlichen Parameter flex_type kann die Anzahl Reihen [grid-cols-x] definiert werden.
-    Die HTML-Struktur wird in den div-Container (rocketpager-{element-slug}) eingefügt. Falls nebst den Standard-Klassen
-    zusätzliche Klassen benötigt werden, können diese über die Variable element_classes mitgegeben werden.
+    Innerhalb von "@section('content-section')" und "@endsection" kann die HTML Struktur des Block definiert werden.
+    Die HTML-Struktur wird in den div-Container (rocketpager-{element-name}) eingefügt. Falls nebst den Standard-Klassen
+    zusätzliche Klassen benötigt werden, können diese über die Variable element_classes mitgegeben werden."
 --}}
 
 @extends('blocks.helpers.preview-wrapper', [
@@ -13,8 +10,6 @@
 ])
 
 @section('content-section-before-flex')
-    {{-- Inhalt für Struktur (Ansicht) bei Normallfall (oder vor wiederholenden Elementen) --}}
-
     <p>Field: {{ block_value('field') }}</p>
     <p>Empty-Field: {{ block_value('empty-field') }}</p>
     <p>No-Field: {{ block_value('no-field') }}</p>
@@ -29,29 +24,21 @@
 @overwrite
 
 @section('flex-item-content')
-    {{-- Inhalt für Struktur (Ansicht) für wiederholenden Elementen (bsp. bei Repeater-Feld oder Query-Loop) --}}
-
-    @if(block_rows('repeater'))
-        @while (block_rows('repeater'))
-            @php block_row('repeater') @endphp {{-- Next Loop with new Repeater-Element --}}
-            <div class="mt-6">
-                <div class="image-wrapper">
-                    @include('blocks.helpers.image',
-                    [
-                        'name_ImageField' => 'repeater-image',
-                        'additionalClasses' => array('class' => 'margin-medium-r'),
-                        'isRepeaterElement' => true
-                    ])
+        @if(block_rows('repeater'))
+            @while (block_rows('repeater'))
+                @php block_row('repeater') @endphp {{-- Next Loop with new Repeater-xl --}}
+                <div class="mt-6">
+                    <div class="image-wrapper">
+                        @include('blocks.helpers.image',
+                        [
+                            'name_ImageField' => 'repeater-image',
+                            'additionalClasses' => array('class' => 'margin-medium-r'),
+                            'isRepeaterElement' => true
+                        ])
+                    </div>
+                    <div class="text-wrapper">Repeater-Item: {{ block_sub_value( 'repeater-item') }}</div>
                 </div>
-                <div class="text-wrapper">Repeater-Item: {{ block_sub_value( 'repeater-item') }}</div>
-            </div>
-        @endwhile
-        {{ reset_block_rows( 'repeater' ) }} {{-- Setzt den Counter des Repeaters zurück --}}
-    @endif
-@overwrite
-
-@section('content-section-after-flex')
-    {{-- Struktur und Darstellung nach wiederholenden Elementen --}}
-
-    <p>Dieser Inhalt wird nacht den wiederholenden Elementen dargestellt.</p>
+            @endwhile
+            {{ reset_block_rows( 'repeater' ) }} {{-- Setzt den Counter des Repeaters zurück --}}
+        @endif
 @overwrite
--- resources/views/partials/content/content-archive.blade.php
+++ resources/views/partials/content/content-archive.blade.php
@@ -1,7 +1,7 @@
 <div class="wp-block-group is-style-layout-full">
     <div class="wp-block-group scroll-reveal anim__fadeInUp">
     <h1>{{ App\pl__('Archiv - Titel') }}</h1>
-        <div class="grid grid-col-1 md:grid-cols-2 lg:grid-cols-3 gap-gutter">
+        <div class="grid grid-col-1 md:grid-cols-2 lg:grid-cols-3 gap-medium">
             @while(have_posts()) @php(the_post())
                 @includeFirst(['partials.content.content-' . get_post_type(), 'partials.content.content'])
             @endwhile
