0f4ae008d7552b9e64a4b98126de1988db9757ee
Patrick Siegfried <siegfried.patrick@gmail.com>
2023-12-05 12:15:41 +0000

    done

--- resources/blocks/rocket-element/src/view.js
+++ resources/blocks/rocket-element/src/view.js
@@ -21,5 +21,5 @@
  */
 
 /* eslint-disable no-console */
-console.log( 'Hello World! (from create-block-rocket-element block)' );
+// console.log( 'Hello World! (from create-block-rocket-element block)' );
 /* eslint-enable no-console */
