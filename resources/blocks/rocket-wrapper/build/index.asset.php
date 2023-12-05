4850f506c75070064a6ffe8aab2f47c97e133434
Patrick Siegfried <siegfried.patrick@gmail.com>
2023-12-05 12:40:12 +0000

    dropped max-ws

--- resources/blocks/rocket-wrapper/build/index.asset.php
+++ resources/blocks/rocket-wrapper/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'f804ab92994ee187edc2');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '30c9d3a97b2de6c841a6');
