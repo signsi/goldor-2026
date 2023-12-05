386833d541f0e65be2499828780d1d698e61b15d
Tim Kaegi <tkaegi@rocket.ch>
2023-12-05 15:53:16 +0000

    Editor und Font-Styles

--- bud.config.js
+++ bud.config.js
@@ -68,17 +68,6 @@ export default async (app) => {
          */
         .wpjson.setSettings({
             appearanceTools: true,
-            // blocks: {
-            //     "core/group": {
-            //         // "__experimentalLayout": true,
-            //         "spacing": {
-            //             "blockGap": [
-            //                 "horizontal",
-            //                 "vertical"
-            //             ]
-            //         }
-            //     }
-            // },
             color: {
                 link: true,
                 custom: true,
