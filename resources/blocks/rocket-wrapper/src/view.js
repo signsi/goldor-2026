4649235c917a34b0e2a72da2f0898461af27aeaa
Patrick Siegfried <siegfried.patrick@gmail.com>
2023-12-05 20:11:15 +0000

    web view ok

--- resources/blocks/rocket-wrapper/src/view.js
+++ resources/blocks/rocket-wrapper/src/view.js
@@ -21,5 +21,5 @@
  */
 
 /* eslint-disable no-console */
-console.log( 'Hello World! (from create-block-rocket-wrapper block)' );
+// console.log( 'Hello World! (from create-block-rocket-wrapper block)' );
 /* eslint-enable no-console */
