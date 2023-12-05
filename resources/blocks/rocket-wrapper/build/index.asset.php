f2e2a00efc3ad3d236355a2700e696755fe9937f
Patrick Siegfried <siegfried.patrick@gmail.com>
2023-12-05 20:11:15 +0000

    web view ok

--- resources/blocks/rocket-wrapper/build/index.asset.php
+++ resources/blocks/rocket-wrapper/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '9659558a8319687ec658');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'ff7f6abdc274e5a7af2b');
