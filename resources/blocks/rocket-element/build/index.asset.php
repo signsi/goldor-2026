3aa0974eda27cd9ee6986bfc0af725bc23e47bde
Patrick Siegfried <siegfried.patrick@gmail.com>
2023-12-05 11:38:35 +0000

    rocket/element working

--- resources/blocks/rocket-element/build/index.asset.php
+++ resources/blocks/rocket-element/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'ec498b7e447050297e42');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'dae605c47f1b8021eebb');
