bd6f768ae6f7cf3952fe3614351c6ee996208733
Luca Rava <lrava@rocket.ch>
2024-01-11 16:23:23 +0000

    Umstrukturierung mit CTA und Vorbereitung fuer dynamische Hoehe

--- resources/views/partials/language/langswitcher-horizontalList.blade.php
+++ resources/views/partials/language/langswitcher-horizontalList.blade.php
@@ -1,8 +1,12 @@
+@php
+    $color = $color ?? 'font';
+@endphp
+
 @if (App\is_plugin_active_and_available('polylang/polylang.php') && has_nav_menu('language_switcher'))
     {!! wp_nav_menu([
         'theme_location' => 'language_switcher',
-        'menu_class' => 'lang-switcher-nav list-none flex flex-row my-0',
+        'menu_class' => 'lang-switcher-nav list-none flex flex-row my-0 pl-0',
         'container_class' => '',
-        'add_li_class' => 'relative text-sm pr-3 pl-0 mr-3 last:pr-0 last:mr-0 border-r border-r-font border-solid last:border-r-0 transition-colors hover:text-primary',
+        'add_li_class' => 'relative text-' . $color .' text-sm pr-3 pl-0 mr-3 last:pr-0 last:mr-0 border-r border-r-' . $color . ' border-solid last:border-r-0 transition-colors hover:text-primary',
     ]) !!}
 @endif
--- resources/views/sections/header/elements/mobile-navigation.blade.php
+++ resources/views/sections/header/elements/mobile-navigation.blade.php
@@ -4,23 +4,53 @@
     $lang_switch_active = App\getThemeOption('header_lang_switcher');
 @endphp
 
-<div id="mobileNav" class="mobileMenuHide {{ $menuSlideFrom }} bg-secondary !ml-0 absolute left-0 right-0 inset-x-0 transition-all duration-500 transform origin-top lg:hidden ease-in-out overflow-y-auto overflow-x-hidden h-vh">
-    <div class="flex flex-col gap-gutter px-gutter py-3xl text-font">
-        <nav>
-            @if (has_nav_menu('primary_navigation'))
-                @php
-                    wp_nav_menu([
-                        'theme_location' => 'primary_navigation',
-                        'menu_class' => 'menu-primary_navigation flex flex-col space-y-4 items-start my-0 px-0',
-                        'container_class' => 'menu-primary_navigation-container',
-                        'add_li_class' => 'relative w-full group text-xl text-white hover:text-font w-min-content',
-                        'add_sub_li_class' => 'text-font',
-                        'walker' => new SubmenuWrap()
-                    ])
-                @endphp
-            @endif
-        </nav>
-        @includeWhen($lang_switch_active, 'partials.language.langswitcher-horizontalList')
-        @includeWhen($search_active, 'forms.search') 
+@php
+    $mail = App\getThemeOption('email');
+    $tel = App\getThemeOption('tel');
+@endphp
+
+
+<div id="mobileNav" class="mobileMenuHide {{ $menuSlideFrom }} bg-secondary !ml-0 absolute left-0 right-0 inset-x-0 transition-all duration-500 transform origin-top lg:hidden ease-in-out overflow-y-auto overflow-x-hidden">
+    <div class="h-[calc(var(--full-height)-75.25px)] bg-theme text-font">
+        <div class="h-[calc(100%-60px)] flex flex-col gap-14 px-gutter pt-10 pb-14 overflow-y-scroll">
+            @includeWhen($search_active, 'forms.search')
+            <nav>
+                @if (has_nav_menu('primary_navigation'))
+                    @php
+                        wp_nav_menu([
+                            'theme_location' => 'primary_navigation',
+                            'menu_class' => 'menu-primary_navigation flex flex-col space-y-4 items-start my-0 px-0',
+                            'container_class' => 'menu-primary_navigation-container',
+                            'add_li_class' => 'relative w-full group text-xl text-white hover:text-font w-min-content',
+                            'add_sub_li_class' => 'text-font',
+                            'walker' => new SubmenuWrap()
+                        ])
+                    @endphp
+                @endif
+            </nav>
+            @includeWhen($lang_switch_active, 'partials.language.langswitcher-horizontalList', ['color' => 'white'])
+        </div>
+        <div class="fixed-bottom">
+            <ul class="flex justify-between w-full p-0 m-0 list-none border-t border-primarylight divide-x divide-primarylight">
+                @if($tel)
+                    <li class="flex-auto w-full p-0 m-0">
+                        <a class="flex items-center justify-center w-full h-full p-5 font-base group hover:bg-primary-hover" href="tel:{{ $tel }}">
+                            <svg class="w-5 h-full" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path class="fill-white group-hover:fill-secondarylight" d="M0 1.5L6.75 0L10.5 6.75L6.57187 9.89062C8.26406 13.0969 10.8984 15.7312 14.1094 17.4281L17.25 13.5L24 17.25L22.5 24H21C9.40313 24 0 14.5969 0 3V1.5Z" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
+                        </a>
+                    </li>
+                @endif
+                @if($mail)
+                    <li class="flex-auto w-full p-0 m-0">
+                        <a class="flex items-center justify-center w-full h-full p-5 font-base group hover:bg-primary-hover" target="blank" href="mailto:{{ $mail }}">
+                            <svg class="w-5 h-full" width="24" height="18" viewBox="0 0 24 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path class="fill-white group-hover:fill-secondarylight" d="M0 0H24V3.75L12 12L0 3.75V0ZM0 18V5.56875L11.1516 13.2375L12 13.8187L12.8484 13.2328L24 5.56875V18H0Z" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
+                        </a>
+                    </li>
+                @endif
+            </ul>
+        </div>
     </div>
 </div>
