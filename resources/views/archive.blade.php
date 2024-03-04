10aa4580b8292a09cb817d5fa4a1a9f71e25a4a3
Tim <tkaegi@rocket.ch>
2024-03-04 08:33:45 +0000

    Test upload

--- resources/views/archive.blade.php
+++ resources/views/archive.blade.php
@@ -12,7 +12,6 @@
     @if (! have_posts())
         @include('partials.content.content-none')
     @else
-        {{-- Versucht, den passenden Inhalt einzufügen --}}
         @includeFirst([
             'partials.' . $post_type . '.content-archive', // z.B. 'partials.post.content-archive' für den Beitragstyp 'post'
             'partials.content.content-archive' // Standard-Inhalt für Archivansicht
